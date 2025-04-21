cb84b11ab3ce09c56f2780c3e6d341e853cd0e2f
vnayak-byte <veda.nayak@gmail.com>
2025-04-21 13:55:11 -0700

    update

--- assignment2functions.R
+++ assignment2functions.R
@@ -78,21 +78,52 @@ extractLine2 = # get the class and priority from a block's line 2
 
 # Extract data from block line 3 -----------------------------------------------
 
-extractLine2 = # BLANK from a block's line 3
+extractLine4 = # month/day/time + IP/port info from a block's line 3
   function(block){
     
     # line3 = block[3]
-    line3 = "03/16-07:30:00.000000 192.168.202.79:50465 -> 192.168.229.251:80"
+    line3 = "03/16-07:30:00.840000 192.168.202.79:50465 -> 192.168.229.251:80"
     
     # day-time =  03/16-08:30:07.840000
     # source IP : port
     # destination IP : port
-    
-    parts = gsub("\\[Classification: (.*)\\] \\[Priority: (.*)\\]", "\\1 ; \\2", line2)
+
+    parts = gsub("([0-9][0-9])\\/([0-9][0-9])-([0-9][0-9]):([0-9][0-9]):([0-9][0-9].[0-9]*) ([0-9]*\\.[0-9]*\\.[0-9]*\\.[0-9]*):([0-9]*) -> ([0-9]*\\.[0-9]*\\.[0-9]*\\.[0-9]*):([0-9]*)", "\\1 ; \\2 ; \\3 ; \\4 ; \\5 ; \\6 ; \\7 ; \\8 ; \\9", line3)
     parts_split = unlist(strsplit(parts, split = " ; ")) 
     
-    # parts_split[1] = classification
-    # parts_split[2] = priority
+    # 1 - month
+    # 2 - day
+    # 3 - hour
+    # 4 - minute
+    # 5 - second.millisecond
+    # 6 - source IP
+    # 7 - source port
+    # 8 - destination IP
+    # 9 - destination port
+    
+    parts_split
+    
+  }
+
+
+# Extract data from block line 4 -----------------------------------------------
+
+extractLine4 = # BLANK info from a block's line 4
+  function(block){
+    
+    # line4 = block[4]
+    line4 = "TCP TTL:64 TOS:0x0 ID:6622 IpLen:20 DgmLen:1420 DF"
+    
+    parts = gsub("([:alpha:]*) TTL:([0-9]*) TOS:([0-9]*x[0-9]*) ID:([0-9]*) IpLen:([0-9]*) DgmLen:([0-9]*) ([:alpha:]*)", "\\1 ; \\2 ; \\3 ; \\4 ; \\5 ; \\6 ; \\7 ", line4)
+    parts_split = unlist(strsplit(parts, split = " ; ")) 
+    
+    # 1 - protocol
+    # 2 - ttl
+    # 3 - tos
+    # 4 - id
+    # 5 - iplen
+    # 6 - dgmlen
+    # 7 - extra
     
     parts_split
     
